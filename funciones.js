34b6fdfdec2a3055da6abfa46baf5b1516717e82
alejo <alejonahuel1831@gmail.com>
2024-01-18 12:16:37 -0300

    menu, logo, salir

--- funciones.js
+++ funciones.js
@@ -20,15 +20,15 @@ function activate(e){
         
             if (++currentMove == 2){
                 currentattempts++;
-                document.querySelector('#stats').innerHTML = currentattempts + ' intentos'
+                document.querySelector('#stats').innerHTML = currentattempts + ' errores'
 
                 if (selectedCards[0].querySelectorAll('.face')[0].innerHTML == selectedCards[1].querySelectorAll('.face')[0].innerHTML){
                     selectedCards = []
                     currentMove = 0
-                    points++;
+                    points+=25;
                     currentattempts--;
-                    document.querySelector('#puntos').innerHTML = points + '0 puntos'
-                    document.querySelector('#stats').innerHTML = currentattempts + ' intentos'
+                    document.querySelector('#puntos').innerHTML = points + ' puntos'
+                    document.querySelector('#stats').innerHTML = currentattempts + ' errores'
 
                 }
                 else{
@@ -73,3 +73,29 @@ while (totalCards % 2 == 1){
 }
 
 
+        // Función que se ejecutará al hacer clic en el enlace
+        function mostrarMensaje() {
+            // Utiliza la función confirm() para mostrar un cuadro de diálogo
+            var confirmacion = confirm("¿Estás seguro que queres salir ? :(");
+
+            // Verifica la respuesta del usuario
+            if (confirmacion) {
+                // Si el usuario hace clic en "Aceptar"
+                window.location.href = enlace.href;
+            } else {
+                // Si el usuario hace clic en "Cancelar" o cierra el cuadro de diálogo
+                
+            }
+        }
+
+        // Obtén el elemento del enlace por su ID
+        var enlace = document.getElementById("salir_juego");
+
+        // Agrega un escuchador de eventos para el clic en el enlace
+        enlace.addEventListener("click", function (event) {
+            // Previene el comportamiento predeterminado del enlace
+            event.preventDefault();
+
+            // Llama a la función para mostrar el mensaje
+            mostrarMensaje();
+        });
